3d3bbe90872cbfdcd7e8032d43073ea49b8164af
onewordFOURWORDS <timdanham@gmail.com>
2022-02-04 16:26:48 +0000

    in class

--- scripts/control_node.py
+++ scripts/control_node.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
 import rospy
-# we are going to read turtlesim/Pose messages this time
+# import turtlesim/Pose to read the turtle position
 from turtlesim.msg import Pose
+
+def pose_callback(data):
+	# get turtle x and y positions
+	turt_x = data.x
+	turt_y = data.y
